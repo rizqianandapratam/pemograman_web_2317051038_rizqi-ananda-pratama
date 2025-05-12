1ca8e026d2c59357b3c3bbf682fadcf1dbd11b11
rizqianandapratam <151344185+rizqianandapratam@users.noreply.github.com>
2025-05-12 21:55:35 +0700

    Add files via upload

---
+++ pertemuan11/form/form login/login.php
@@ -0,0 +1,21 @@
+<?php 
+session_start(); 
+include 'koneksi/db.php'; 
+
+$username = $_POST['username']; 
+$password = $_POST['password']; 
+
+$query = "SELECT * FROM users WHERE username='$username'"; 
+$result = mysqli_query($conn, $query); 
+$user = mysqli_fetch_assoc($result); 
+
+if ($user && password_verify($password, $user['password'])) { 
+    $_SESSION['user'] = $user['username']; 
+    header("Location: dashboard.php"); 
+    exit;
+} else { 
+    $_SESSION['error'] = "Login gagal. Username atau password salah."; 
+    header("Location: index.php");
+    exit;
+}
+?>
